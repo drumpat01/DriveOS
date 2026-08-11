52967b49d0bdb2a12f17ce6410d9fcc7161e334f
Patrick Stewart <pstewart@firstrate.com>
2026-08-11 15:55:59 -0500

    Complete Timeline UI and responsive dashboard deployment

--- web/core/build.js
+++ web/core/build.js
@@ -1,4 +1,4 @@
 (function () {
-  window.DriveOSBuild = Object.freeze({ version: "5.0.0", webBuild: "5.0.0" });
+  window.DriveOSBuild = Object.freeze({ version: "5.0.0", webBuild: "5.0.1" });
   document.documentElement.dataset.webBuild = window.DriveOSBuild.webBuild;
 })();
