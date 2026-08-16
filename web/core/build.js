790a96e15941ab1d0a390b2e992504fb4304b714
Patrick Stewart <patrickbstewart@gmail.com>
2026-08-15 23:19:53 -0500

    Release JourneyDeck 5.3.1 header fix

--- web/core/build.js
+++ web/core/build.js
@@ -1,4 +1,4 @@
 (function () {
-  window.DriveOSBuild = Object.freeze({ version: "5.3.0", webBuild: "5.3.0" });
+  window.DriveOSBuild = Object.freeze({ version: "5.3.1", webBuild: "5.3.1" });
   document.documentElement.dataset.webBuild = window.DriveOSBuild.webBuild;
 })();
