8a4b704594bc13a0cb9e2d2166c0845fdd50a84e
Patrick Stewart <patrickbstewart@gmail.com>
2026-08-16 13:42:10 -0500

    Anchor collection actions to viewport

--- desktop/Program.cs
+++ desktop/Program.cs
@@ -16,9 +16,9 @@ using Microsoft.Web.WebView2.WinForms;
 [assembly: AssemblyTitle("JourneyDeck")]
 [assembly: AssemblyProduct("JourneyDeck")]
 [assembly: AssemblyDescription("Personal Vehicle Intelligence")]
-[assembly: AssemblyVersion("5.9.2.0")]
-[assembly: AssemblyFileVersion("5.9.2.0")]
-[assembly: AssemblyInformationalVersion("5.9.2")]
+[assembly: AssemblyVersion("5.9.3.0")]
+[assembly: AssemblyFileVersion("5.9.3.0")]
+[assembly: AssemblyInformationalVersion("5.9.3")]
 
 namespace DriveOSDesktop
 {
--- web/service-worker.js
+++ web/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "driveos-shell-5.9.2";
+const CACHE_NAME = "driveos-shell-5.9.3";
 const OFFLINE_URL = "/offline.html";
 
 const SHELL = [
