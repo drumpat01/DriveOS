777440c80590838f5e26e472d033d17b4e290fa8
Patrick <patrickbstewart@gmail.com>
2026-08-16 08:10:57 -0500

    Refresh stylesheet build version (#55)

--- desktop/Program.cs
+++ desktop/Program.cs
@@ -16,9 +16,9 @@ using Microsoft.Web.WebView2.WinForms;
 [assembly: AssemblyTitle("JourneyDeck")]
 [assembly: AssemblyProduct("JourneyDeck")]
 [assembly: AssemblyDescription("Personal Vehicle Intelligence")]
-[assembly: AssemblyVersion("5.6.1.0")]
-[assembly: AssemblyFileVersion("5.6.1.0")]
-[assembly: AssemblyInformationalVersion("5.6.1")]
+[assembly: AssemblyVersion("5.6.2.0")]
+[assembly: AssemblyFileVersion("5.6.2.0")]
+[assembly: AssemblyInformationalVersion("5.6.2")]
 
 namespace DriveOSDesktop
 {
--- web/service-worker.js
+++ web/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "driveos-shell-5.6.1";
+const CACHE_NAME = "driveos-shell-5.6.2";
 const OFFLINE_URL = "/offline.html";
 
 const SHELL = [
